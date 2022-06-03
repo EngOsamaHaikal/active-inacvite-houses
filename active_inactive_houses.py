c6e814ff1f28395261972ec4ae887cf0a0e6ef31
Osama Abdallah Issa Haikal <72370395+OsamaHaikal@users.noreply.github.com>
2022-06-03 22:58:16 +0300

    Update active_inactive_houses.py

--- active_inactive_houses.py
+++ active_inactive_houses.py
@@ -1,21 +1,18 @@
 def stateHouses(states,days):
     final_states=[0]*len(states)
+
     while days>0:
-        
         for i in range(len(states)):
             if i==0:
                 a=0^states[i+1]
-                final_states[i]=a
             elif i==(len(states)-1):
                 a=0^states[i-1]
-                final_states[i]=a
             else:
                 a=states[i+1]^states[i-1]
-                final_states[i]=a
-                
+            final_states[i]=a
         states=final_states
         final_states=[0]*len(states)
-        days-=1        
+        days-=1
     return states
 
-print(stateHouses([1,0,0,0,0,1,0,0],1))                   
+print(stateHouses([1,0,0,0,0,1,0,0],4))
